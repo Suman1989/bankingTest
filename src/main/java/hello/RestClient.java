0d6e7a271d55d37c0aaab1fedc5093101968c198
Hirak17 <38650532+Hirak17@users.noreply.github.com>
2018-04-25 14:27:05 +0530

    Update RestClient

--- src/main/java/hello/RestClient.java
+++ src/main/java/hello/RestClient.java
@@ -1,5 +1,24 @@
 package hello;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+
+
+@Service
 public class RestClient{
 
+  @Autowired
+	private RestTemplate restTemplate;
+  
+  public void callApi(){
+    
+  }
 }
