77bc7ec817b957fc6eacaa488fe5d043b3182562
Suman1989 <38751588+Suman1989@users.noreply.github.com>
2018-04-26 16:16:13 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -19,13 +19,13 @@ public class HelloWorldController {
 	private WebhookJsonConverter webhookJsonConverter;
 
 	@RequestMapping(method = {RequestMethod.POST})
-	public @ResponseBody WebhookResponse webhookPost(@RequestBody String jsonInString){
+	public @ResponseBody WebhookResponse webhookPost(@RequestBody Request request){
 		//jsonInString="{\"id\":\"9f492f90-f949-48f1-9137-3ff1c8913f7a\",\"timestamp\":\"2018-04-26T04:03:54.626Z\",\"lang\":\"en\",\"result\":{\"source\":\"agent\",\"resolvedQuery\":\"tell me about the market update\",\"speech\":\"\",\"action\":\"personalhelp\",\"actionIncomplete\":false,\"parameters\":{\"ConversationType\":\"IN\"},\"contexts\":[],\"metadata\":{\"intentId\":\"327debc4-7c47-4925-9ea7-ac9b2ef976a1\",\"webhookUsed\":\"true\",\"webhookForSlotFillingUsed\":\"true\",\"intentName\":\"PersonalHelper\"},\"fulfillment\":{\"speech\":\"Hi response static\",\"messages\":[{\"type\":0,\"speech\":\"Hi response static\"}]},\"score\":1.0},\"status\":{\"code\":200,\"errorType\":\"success\",\"webhookTimedOut\":false},\"sessionId\":\"d0e16ee8-1330-44b3-93c3-f62d8583c333\"}";
-		System.out.println(jsonInString);
+		//System.out.println(jsonInString);
 		//String strISO=webhookJsonConverter.convert(jsonInString);
 		//strISO = restClient.callISOApi(strISO);
 		//System.out.println("************ISO Code is "+strISO);
-		return new WebhookResponse("Hello! Hirak...." + jsonInString, "Text " + jsonInString);
+		return new WebhookResponse("Hello! Hirak...." + request.getResult().getResolvedQuery(), "Text " + request.getResult().getResolvedQuery());
 	}	
 	
 }
