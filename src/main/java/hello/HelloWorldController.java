addf2607a2c4ec3769a0e4a1b5447f1a632d39df
Suman1989 <38751588+Suman1989@users.noreply.github.com>
2018-04-26 16:50:48 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import hello.RestClient;
-import hello.model.*;
+import hello.request.WebhookRequest;
 
 @Controller
 @RequestMapping("/webhook")
@@ -20,7 +20,7 @@ public class HelloWorldController {
 	private WebhookJsonConverter webhookJsonConverter;
 
 	@RequestMapping(method = {RequestMethod.POST})
-	public @ResponseBody WebhookResponse webhookPost(@RequestBody Request request){
+	public @ResponseBody WebhookResponse webhookPost(@RequestBody WebhookRequest request){
 		//jsonInString="{\"id\":\"9f492f90-f949-48f1-9137-3ff1c8913f7a\",\"timestamp\":\"2018-04-26T04:03:54.626Z\",\"lang\":\"en\",\"result\":{\"source\":\"agent\",\"resolvedQuery\":\"tell me about the market update\",\"speech\":\"\",\"action\":\"personalhelp\",\"actionIncomplete\":false,\"parameters\":{\"ConversationType\":\"IN\"},\"contexts\":[],\"metadata\":{\"intentId\":\"327debc4-7c47-4925-9ea7-ac9b2ef976a1\",\"webhookUsed\":\"true\",\"webhookForSlotFillingUsed\":\"true\",\"intentName\":\"PersonalHelper\"},\"fulfillment\":{\"speech\":\"Hi response static\",\"messages\":[{\"type\":0,\"speech\":\"Hi response static\"}]},\"score\":1.0},\"status\":{\"code\":200,\"errorType\":\"success\",\"webhookTimedOut\":false},\"sessionId\":\"d0e16ee8-1330-44b3-93c3-f62d8583c333\"}";
 		//System.out.println(jsonInString);
 		//String strISO=webhookJsonConverter.convert(jsonInString);
