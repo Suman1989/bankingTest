f35a38439b1f2e8c6b3d8af0cc8ee2e15112e4aa
Suman1989 <38751588+Suman1989@users.noreply.github.com>
2018-04-27 11:47:36 +0530

    Update WebhookConfig.java

--- src/main/java/hello/WebhookConfig.java
+++ src/main/java/hello/WebhookConfig.java
@@ -14,23 +14,72 @@ import org.apache.http.client.HttpClient;
 @Configuration
 public class WebhookConfig {
 	
-	@Bean
-	public RestTemplate restTemplate()  {
-		RestTemplate restTemplate = null;
-		try {
-			restTemplate = new RestTemplate(getClientHttpRequestFactory());
-			System.out.println("******Rest Template created*****");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	@Value(value = "classpath:certs/00DS0000003Kwz9.jks")
+	private Resource serverSSLKeyStore;
 	
-	      return restTemplate;
+	@Bean
+	@Qualifier("restTemplateOpen")
+	public RestTemplate restTemplateOpen() throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException  {
+		
+	    TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+	        @Override
+	        public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+	            return true;
+	        }
+	    };
+	    SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+	    SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier(){
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}});
+	    //Proxy only reqd. if calling from Citi network
+		//HttpHost httpHost = new HttpHost("webproxy.wlb2.nam.nsroot.net", 8080, "http");
+		//HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).setProxy(httpHost).build();
+	    CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+	    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+	    requestFactory.setHttpClient(httpClient);	  
+	    RestTemplate restTemplate = new RestTemplate(requestFactory);
+	    SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslSocketFactory); 
+        SSLContext.setDefault(sslContext);
+	    return restTemplate;
+	}
+		
+	@Bean
+	@Qualifier("restTemplate")
+	public RestTemplate restTemplate() throws UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException  {
+		
+		RestTemplate restTemplate = new RestTemplate(setTrustStore());
+		return restTemplate;
 	}
 	
-public ClientHttpRequestFactory getClientHttpRequestFactory()  {
+	@Bean
+	@Qualifier("restTemplateBasic")
+	public RestTemplate restTemplateBasic(){
 		
-        HttpClient httpClient = HttpClients.custom().build();
-        
-	    return new HttpComponentsClientHttpRequestFactory(httpClient);
+		return new RestTemplate();
+	}
+	
+	private ClientHttpRequestFactory setTrustStore() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException, KeyManagementException{
+		
+		String keyStorePassword = "sfdc123";
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+	    keyStore.load(serverSSLKeyStore.getInputStream(), keyStorePassword.toCharArray());
+
+	    SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+	            new SSLContextBuilder()
+	                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+	                    .loadKeyMaterial(keyStore, keyStorePassword.toCharArray())
+	                    .build(),
+	            NoopHostnameVerifier.INSTANCE);
+
+	    //Proxy only reqd. if calling from Citi network
+	   // HttpHost httpHost = new HttpHost("webproxy.wlb2.nam.nsroot.net", 8080, "http");
+	    HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+
+	    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+	    return requestFactory;   
+		
 	}
 }
